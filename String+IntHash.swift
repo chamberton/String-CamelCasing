9869001f5bbb1e1da5109f8fd809850cd2add2b5
Serge Mbamba <serge.mbamba@momentum.co.za>
2018-10-09 13:46:20 +0200

    moved into github

---
+++ String+IntHash.swift
@@ -0,0 +1,18 @@
+// http://www.cse.yorku.ca/~oz/hash.html
+
+extension String {
+    var djb2Hash: Int {
+        let unicodeScalars = self.unicodeScalars.map { $0.value }
+        return unicodeScalars.reduce(5381) {
+            ($0 << 5) &+ $0 &+ Int($1)
+        }
+    }
+    
+  
+    var sdbmHash: Int {
+        let unicodeScalars = self.unicodeScalars.map { $0.value }
+        return unicodeScalars.reduce(0) {
+            Int($1) &+ ($0 << 6) &+ ($0 << 16) - $0
+        }
+    }
+}
